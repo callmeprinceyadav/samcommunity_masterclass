de5634376b013159af5e4d352b77cc26b329fe55
Prince Kumar <139939808+callmeprinceyadav@users.noreply.github.com>
2025-11-17 14:21:03 +0530

    edited

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,37 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   // Image optimization for Netlify
+//   images: {
+//     unoptimized: false,
+//     remotePatterns: [],
+//   },
+// }
+
+// export default nextConfig
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Image optimization for Netlify
+
   images: {
     unoptimized: false,
-    remotePatterns: [],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+        port: "",
+        pathname: "/uc",
+      },
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+        port: "",
+        pathname: "/uc*",
+      },
+    ],
   },
-}
+};
 
-export default nextConfig
-
-
+export default nextConfig;
